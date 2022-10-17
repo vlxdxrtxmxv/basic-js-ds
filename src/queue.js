326265b159b6c4d6165d6c74e7472dc759ee0cdd
vlxdxrtxmxv <103958051+vlxdxrtxmxv@users.noreply.github.com>
2022-10-17 22:29:07 +0300

    Update queue.js

--- src/queue.js
+++ src/queue.js
@@ -13,6 +13,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+
+class ListNode {
+  constructor(x) {
+    this.value = x;
+    this.next = null;
+  }
+}
+ 
 class Queue {
 constructor(){
   this.head = null;
