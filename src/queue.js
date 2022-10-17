8591ed3034a1e6c4d102b36532158eb0f0e57b12
vlxdxrtxmxv <103958051+vlxdxrtxmxv@users.noreply.github.com>
2022-10-17 22:24:10 +0300

    Update queue.js

--- src/queue.js
+++ src/queue.js
@@ -14,20 +14,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
+constructor(){
+  this.head = null;
+}
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.head;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+   if(this.head === null)
+   {
+     this.head = new ListNode(value)
+   }
+    else{
+      let cur = this.head;
+      while(cur.next)
+        {
+          cur = cur.next;
+        }
+      cur.next = new ListNode(value);
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let last = this.head;
+    this.head = this.head.next;
+    return last.value;
   }
 }
 
